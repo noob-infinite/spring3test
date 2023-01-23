09e62b3b2d97e7f09bef65d406af75d248e19802
noob <compsuntana@gmail.com>
2023-01-23 14:33:52 +0700

    First Commit

---
+++ src/main/java/com/osuntana/java/spring3test/Spring3testApplication.java
@@ -0,0 +1,13 @@
+package com.osuntana.java.spring3test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring3testApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osuntana/java/spring3test/Spring3testApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osuntana.java.spring3test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3testApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
